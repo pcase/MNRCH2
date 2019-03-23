7a5c6cccda65d95a89a5d5daef8bc876fa4a6594
Patty Case <beadypatty@yahoo.com>
2019-03-23 14:02:25 -0700

    Initial Commit

---
+++ MNRCH2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MNRCH2
+//
+//  Created by Patty Case on 3/23/19.
+//  Copyright © 2019 Azure Horse Creations. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
